48db0504a81eedf5ff59503fc937791b2a3ed348
Jackson Palmer <hello@ummjackson.com>
2016-01-17 15:36:45 -0800

    Scrollbar fix...

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ var BrowserWindow = electron.BrowserWindow;
 let mainWindow;
 
 function createWindow () {
-	mainWindow = new BrowserWindow({title: 'Peach Desktop', icon: path.join(__dirname, 'icon.png'), width: 400, height: 600, resizable: false, titleBarStyle: 'default', show: true});
+	mainWindow = new BrowserWindow({title: 'Peach Desktop', icon: path.join(__dirname, 'icon.png'), width: 400, height: 600, resizable: false, titleBarStyle: 'default', show: true, webPreferences: { blinkFeatures: 'OverlayScrollbars', overlayScrollbars: true } });
 	mainWindow.loadURL('http://localhost:1134/index.html');
 	mainWindow.on('closed', function() {
 		mainWindow = null;
